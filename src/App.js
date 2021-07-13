25763b7f6ec9b29235b309c6b809986b2fc41c81
Amutha Muhunthan <amuthamuhunthan@Amuthas-iMac.local>
2021-07-13 16:03:19 +1000

    Test landing Image1

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import IMAGES from "./Images/Images";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Stella Krypriotis's Art</h1>
+        <div className="landingImage">
+          {/* <img src={IMAGES.beachWaves} alt="crashing waves" /> */}
+          <img src={IMAGES.lily} alt="crashing waves" />
+        </div>
       </header>
     </div>
   );
---
+++ src/Images/Images.js
@@ -0,0 +1,6 @@
+const IMAGES = {
+  beachWaves: require("./crashingwaves.jpg").default,
+  majesty: require("./majesty.jpg").default,
+  lily: require("./Lily.jpg").default,
+};
+export default IMAGES;
