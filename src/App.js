34dd8f066b9692e0b3c495aa6fea1d8e6226ec6b
Amutha Muhunthan <amuthamuhunthan@Amuthas-iMac.local>
2021-07-14 13:56:17 +1000

    Test1 image slide

--- src/App.js
+++ src/App.js
@@ -1,16 +1,38 @@
 import "./App.css";
 import IMAGES from "./Images/Images";
+// import Picture1 from "./img/slider/Fuji_TallHero_Beauty_v2_en_US_1x._CB429089975_.jpg";
+
+// import Picture2 from "./img/slider/Fuji_TallHero_Computers_1x._CB432469755_.jpg";
+
+// import Picture3 from "./img/slider/Fuji_TallHero_Currency_v2_en_US_2x._CB428993290_.jpg";
+// import Picture4 from "./img/slider/Fuji_TallHero_Home_v2_en_US_1x._CB429090084_.jpg";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Stella Krypriotis's Art</h1>
-        <div className="landingImage">
-          {/* <img src={IMAGES.beachWaves} alt="crashing waves" /> */}
-          <img src={IMAGES.lily} alt="crashing waves" />
+        <p>Stella Kypriotis Fine Art Jeff</p>
+
+        <div className="landing_Image">
+          <div className="landing_Container">
+            <p>Captivated by Nature</p>
+
+            {/* <img src={IMAGES.beachWaves} alt="crashing waves" /> */}
+            <img src={IMAGES.lily} alt="crashing waves" />
+          </div>
         </div>
       </header>
+      {/* slide  */}
+      {/* <!-- slide show starts here --> */}
+      <div id="slider1">
+        <figure>
+          <img src={IMAGES.lily} alt="" />
+          <img src={IMAGES.majesty} alt="" />
+          <img src={IMAGES.lilyy} alt="" />
+          <img src={IMAGES.crashingwaves} alt="" />
+        </figure>
+      </div>
+      {/* <!-- slide show ends here --> */}
     </div>
   );
 }
