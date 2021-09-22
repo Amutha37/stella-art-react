871979b6b20b9f31a761daf9d5f11835c55de8dc
Amutha Muhunthan <amuthamuhunthan@Amuthas-iMac.local>
2021-09-23 08:50:20 +1000

    tidy up

--- src/App.js
+++ src/App.js
@@ -10,8 +10,6 @@ import Navbar from "./components/Navbar/Navbar";
 
 import Home from "./components/pages/Home";
 
-import Painting from "./components/pages/Painting";
-
 import About from "./components/pages/About";
 import Contact from "./components/pages/Contact";
 
@@ -32,26 +30,25 @@ const App = () => {
       </div> */}
       <Router>
         <Navbar />
-        {/* <Navapp /> */}
-        {/* <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/about" component={About} />
-          <Route exact path="/blog" component={Blog} />
-          <Route exact path="/contact" component={Contact} />
-          
-        </Switch> */}
+
         {/* new  */}
         <Switch>
           <Route path="/" exact component={Home} />
-          <Route path="/paintingsale" component={Painting} />
+
           <Route path="/about" component={About} />
+
           <Route path="/contact" component={Contact} />
-          {/* <Route path="/sign-up" component={SignUp} /> */}
+
           <Route path="/nature" component={Nature} />
+
           <Route path="/portraits" component={Portraits} />
+
           <Route path="/florals" component={Florals} />
+
           <Route path="/birdanimal" component={BirdAnimal} />
+
           <Route path="/commissions" component={Sold} />
+
           {/* home page */}
           <Route>
             <Redirect exact from="/" to="/home" />
