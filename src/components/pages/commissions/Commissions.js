9d2e8d08d97898dd5be7ea30087f0d36241f9c57
Amutha <amuthamuhunthan@Amuthas-iMac.local>
2021-11-10 18:27:01 +1100

    Add testimonial

--- src/components/pages/commissions/Commissions.js
+++ src/components/pages/commissions/Commissions.js
@@ -5,17 +5,42 @@ import './commission.css'
 
 function Commissions() {
   return (
-    <>
+    <div className='commission-container'>
       {/* blury background */}
-      <div>
-        <div class='bg-image'></div>
+      <div className='hero-image-container'>
+        <div className='bg-image'></div>
 
-        <div class='bg-text'>
+        <div className='bg-text'>
           <h1>I am Stella Krypriotis</h1>
           <p>And I'm an Artist</p>
         </div>
       </div>
 
+      <div className='testimonials'>
+        <h1>Testimonials</h1>
+        <br></br>
+        <p>
+          "Watching Stella evolve as a person, as a woman and as an artist,
+          through her artwork, has been an absolute delight. For me, it was not
+          a matter of whether I would own one of Stella's paintings, there was
+          never any doubt about that. The only question I had, was which one I
+          would connect with, so I waited while, feeling my way to the one for
+          me. As soon as I saw the old turtle painting, I knew! It jumped out of
+          the computer screen and it seemed that it actually did speak to me,
+          and I immediately connected!"
+        </p>
+        <br></br>
+
+        <p>
+          "Stella's art has a kind of extra magic, a way of speaking out as
+          though tellling a story, and I love that one can feel the painting as
+          though it's breathing, filed with life. Love the art, love the
+          artist."
+          <br></br>
+          <br></br>~ Themis Thomas ~
+        </p>
+      </div>
+      <hr />
       {/* hero image text */}
       {/* <div
         className='hero-image'
@@ -27,7 +52,7 @@ function Commissions() {
       </div> */}
 
       {/* <Footer /> */}
-    </>
+    </div>
   )
 }
 
