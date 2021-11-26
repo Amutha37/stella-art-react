ae529fdb863ee11c386f2fc56875b9c975f9cf7b
Amutha <amuthamuhunthan@Amuthas-iMac.local>
2021-11-26 17:41:59 +1100

    add ux for testimonial media screen

--- src/components/fetchMessage/Testimonial.jsx
+++ src/components/fetchMessage/Testimonial.jsx
@@ -14,11 +14,11 @@ export function Testimonial() {
         paint something which had a special meaning behind it. We eventually
         came up with this idea of a travelling road leading me towards the light
         and guided by the dove.
-      </p>
-
-      <p>
-        I absolutely love how this painting makes me feel, it brightens my
-        living room and makes me feel at peace.
+        {/* </p> */}
+        <br></br>
+        <br></br>
+        {/* <p> */}I absolutely love how this painting makes me feel, it
+        brightens my living room and makes me feel at peace.
         <br></br>
         <br></br>~ Thank you Stella ~
       </p>
@@ -36,7 +36,10 @@ export function Testimonial() {
         through her colourful, warm brush strokes. She extracts her subject's
         defining characteristics and transforms them into peaceful, realistic
         and stunning works of art."
+        {/* </p> */}
         <br></br>
+        <br></br>
+        {/* <p> */}
         "Thank you Stella Kypriotis for sharing with us your heartfelt creative
         talent."
         <br></br>
@@ -50,24 +53,29 @@ export function Testimonial() {
       <br></br>
       <br></br>
       <img src={deepblueseaturtle} alt='...' height='' width='' />
-      <p>
-        "Watching Stella evolve as a person, as a woman and as an artist,
-        through her artwork, has been an absolute delight. For me, it was not a
-        matter of whether I would own one of Stella's paintings, there was never
-        any doubt about that. The only question I had, was which one I would
-        connect with, so I waited while, feeling my way to the one for me. As
-        soon as I saw the old turtle painting, I knew! It jumped out of the
-        computer screen and it seemed that it actually did speak to me, and I
-        immediately connected!"
-      </p>
+      <div className='testimonial-note'>
+        <p>
+          "Watching Stella evolve as a person, as a woman and as an artist,
+          through her artwork, has been an absolute delight. For me, it was not
+          a matter of whether I would own one of Stella's paintings, there was
+          never any doubt about that. The only question I had, was which one I
+          would connect with, so I waited while, feeling my way to the one for
+          me. As soon as I saw the old turtle painting, I knew! It jumped out of
+          the computer screen and it seemed that it actually did speak to me,
+          and I immediately connected!"
+          {/* </p> */}
+          <br></br>
+          <br></br>
+          {/* <p> */}
+          "Stella's art has a kind of extra magic, a way of speaking out as
+          though tellling a story, and I love that one can feel the painting as
+          though it's breathing, filed with life. Love the art, love the
+          artist."
+          <br></br>
+          <br></br>~ Themis Thomas ~
+        </p>
+      </div>
 
-      <p>
-        "Stella's art has a kind of extra magic, a way of speaking out as though
-        tellling a story, and I love that one can feel the painting as though
-        it's breathing, filed with life. Love the art, love the artist."
-        <br></br>
-        <br></br>~ Themis Thomas ~
-      </p>
       {/* <hr /> */}
       <br></br>
       <br></br>
@@ -78,13 +86,13 @@ export function Testimonial() {
       <p>
         "Such beautiful pieces of Australiana, Stella seems to capture such
         depth into the beautiful creatures and scenic wonders of nature!!
+        <br></br>
+        <br></br>
         Absolutely thrilled to own her artwork and show it off! Every single
         piece is just one of a kind, and we are lucky to own a few!"
-      </p>
-
-      <p>
-        {' '}
-        "Thank you"
+        {/* </p> */}
+        <br></br>
+        {/* <p> */} "Thank you"
         <br></br>
         <br></br>~ Chantelle ~
       </p>
