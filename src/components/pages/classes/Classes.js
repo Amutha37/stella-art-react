a4a127d9aaf0d6b1f349cf959eeb5fa81f7ccdc7
Amutha Muhunthan <tamamutha@gmail.com>
2022-03-07 16:38:13 +1100

    change text on classes

--- src/components/pages/classes/Classes.js
+++ src/components/pages/classes/Classes.js
@@ -1,33 +1,36 @@
-import Footer from '../../Footer/Footer'
+import Footer from "../../Footer/Footer";
 
-import './classes.css'
+import "./classes.css";
 
 const Classes = () => {
   return (
     <>
-      <div className='classes-main-container'>
-        <div className='bg-image'>
-          <header className='bg-text'>
+      <div className="classes-main-container">
+        <div className="bg-image">
+          <header className="bg-text">
             <p>Being creative is not a hobby, it is a way of life. </p>
-
-
           </header>
         </div>
       </div>
-       
-      <div id="class_details">
-      <h2>
-    NEW IN 2022
-      </h2>
 
-<p>
-Children Art Classes will be running shortly in school terms.
-</p>
-<p>For further information I will be giving more details shortly.</p>
-</div>
+      <div id="class_details">
+        <h3>ART CLASSES</h3>
+
+        <p>
+          Starting in TERM 2 for children aged 8+ in Sydney south-west
+          (Campbelltown area).
+        </p>
+        <div>
+          <p>
+            I will be running children's art classes from my ART-STUDIO located
+            in Sydney south-west (Cambelltown) area.
+          </p>
+          <p>More information will be posted here on 31st MARCH 2022.</p>
+        </div>
+      </div>
       <Footer />
     </>
-  )
-}
+  );
+};
 
-export default Classes
+export default Classes;
